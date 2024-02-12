8ace2f2e16249b90a7f2137070c70d39c2f61848
Dude <dudeblogger1002gmail.com>
2024-02-12 17:32:03 +0530

    Second Commit "I have done with : 1)Project structure 2)Convex & Clerk 3)Dashboard Layout 4)Sidebar 5)Organization Sidebar"
    date: 12-feb-24

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashBoardPage = () => {
+  return <div>Dashboard Root Page</div>;
+};
+
+export default DashBoardPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://summary-iguana-58.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
