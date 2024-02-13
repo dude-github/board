6abc68f4bd91e5c044bf7ae82bc8fa1fe846bd5a
Dude <dudeblogger1002gmail.com>
2024-02-13 13:33:28 +0530

    Forth Commit : I have done with Empty States " 1) I have created the Empty board states. 2) And i am done with empty states of board. "

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import {
-  OrganizationSwitcher,
   UserButton,
+  OrganizationSwitcher,
   useOrganization,
 } from "@clerk/nextjs";
+
 import { SearchInput } from "./search-input";
 import { InviteButton } from "./invite-button";
 
 export const Navbar = () => {
   const { organization } = useOrganization();
+
   return (
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
