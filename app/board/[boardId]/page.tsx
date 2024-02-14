247543a33aaf376fd9cb8f077d81ac386062f9f7
Dude <dudeblogger1002gmail.com>
2024-02-14 16:14:24 +0530

    Tenth Commit : "Implemented Canvas Layout"

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,12 @@
+import { Canvas } from "./_components/canvas";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  return <Canvas />;
+};
+
+export default BoardIdPage;
