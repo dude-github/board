ba038382c632c841f9f3bd420f7fb6ddce445cee
Dude <dudeblogger1002gmail.com>
2024-02-14 17:09:13 +0530

    Eleventh Commit : Implemented Liveblocks Setup, incorporating the following steps : " 1) Installed Liveblocks and configured its setup to facilitate real-time collaboration functionalities. 2) Configured room IDs to correspond with board IDs, ensuring proper synchronization between rooms and respective boards. 3) Implemented loading functionality to manage the initial loading process effectively. 4) Enhanced user experience by creating skeleton loading components for all elements within the canvas, optimizing performance and visual feedback during loading transitions. "

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-e-md p-3 items-center shadow-md">
       List of users
     </div>
+  );
+};
+
+Participants.Skeleton = function Participants() {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-e-md p-3 items-center shadow-md w-[100px]" />
   );
 };
--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,4 +1,6 @@
+import { Room } from "@/components/room";
 import { Canvas } from "./_components/canvas";
+import { Loading } from "./_components/loading";
 
 interface BoardIdPageProps {
   params: {
@@ -6,7 +8,11 @@ interface BoardIdPageProps {
   };
 }
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
-  return <Canvas />;
+  return (
+    <Room roomId={params.boardId} fallback={<Loading />}>
+      <Canvas boardId={params.boardId} />
+    </Room>
+  );
 };
 
 export default BoardIdPage;
