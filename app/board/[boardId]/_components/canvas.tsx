e97930629478cf4b5e923a1e6922494c734653a8
Dude <dudeblogger1002gmail.com>
2024-02-15 17:48:13 +0530

    17th Commit : Implemented Cursor Presence Feature, enabling real-time rendering of other users' cursors and names : " 1) Utilized SVG and <g> tags to render cursor presence, ensuring smooth interaction despite multiple users simultaneously utilizing the feature. 2) Developed a mutation for mouse pointer events to facilitate the display of all users' cursors. 3) Incorporated throttle functionality to optimize user experience by smoothing cursor movements on the board. 4) Enhanced the feature by displaying users' names alongside their cursors, providing additional context and clarity. 5) Implemented a function to automatically remove cursors when users exit the canvas, maintaining a clean and uncluttered interface. "

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { CanvasMode, CanvasState } from "@/types/canvas";
-import { useHistory, useCanUndo, useCanRedo } from "@/liveblocks.config";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
+import {
+  useHistory,
+  useCanUndo,
+  useCanRedo,
+  useMutation,
+} from "@/liveblocks.config";
+import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 interface CanvasProps {
   boardId: string;
@@ -19,9 +26,37 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     mode: CanvasMode.None,
   });
 
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    // console.log({ x: e.deltaX, y: e.deltaY });
+
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY,
+    }));
+  }, []);
+
+  const onPointerMove = useMutation(
+    ({ setMyPresence }, e: React.PointerEvent) => {
+      e.preventDefault();
+
+      const current = pointerEventToCanvasPoint(e, camera);
+
+      // console.log({ current });
+
+      setMyPresence({ cursor: current });
+    },
+    []
+  );
+
+  const onPointerLeave = useMutation(({ setMyPresence }) => {
+    setMyPresence({ cursor: null });
+  }, []);
 
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
@@ -35,6 +70,16 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+      <svg
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+      >
+        <g>
+          <CursorsPresence />
+        </g>
+      </svg>
     </main>
   );
 };
